7bc962dd8eb7ff7372927bcc15763c39290d331c
gentoo90 <gentoo90@gmail.com>
2015-11-27 20:23:30 +0200

    Documentation for v0.3.3

---
+++ v0.3.3/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['winreg'] = ["impl&lt;'key&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='winreg/struct.EnumKeys.html' title='winreg::EnumKeys'>EnumKeys</a>&lt;'key&gt;","impl&lt;'key&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='winreg/struct.EnumValues.html' title='winreg::EnumValues'>EnumValues</a>&lt;'key&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.3.3/implementors/rustc_serialize/serialize/trait.Decoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['winreg'] = ["impl <a class='trait' href='http://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='winreg/serialization/struct.Decoder.html' title='winreg::serialization::Decoder'>Decoder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
