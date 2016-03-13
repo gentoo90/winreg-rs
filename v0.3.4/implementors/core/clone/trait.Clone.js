d49dbeb34c802afb8174ac58c93bcbfcf9b04427
gentoo90 <gentoo90@gmail.com>
2016-03-13 19:13:26 +0200

    Documentation for v0.3.4

---
+++ v0.3.4/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['winreg'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='winreg/enums/enum.RegType.html' title='winreg::enums::RegType'>RegType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
