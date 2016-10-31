4524aa0d23144a6c527de99824cf0cdb93959b5d
gentoo90 <gentoo90@gmail.com>
2016-10-31 22:40:56 +0200

    Documentation for v0.4.0

---
+++ v0.4.0/implementors/rustc_serialize/serialize/trait.Encoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["winreg"] = ["impl <a class='trait' href='https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encoder.html' title='rustc_serialize::serialize::Encoder'>Encoder</a> for <a class='struct' href='winreg/serialization/struct.Encoder.html' title='winreg::serialization::Encoder'>Encoder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
