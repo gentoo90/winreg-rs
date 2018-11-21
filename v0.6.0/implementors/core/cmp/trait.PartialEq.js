2d392c3119707cb3ece7dac27e8ecc900cf68265
gentoo90 <gentoo90@gmail.com>
2018-11-21 23:27:36 +0200

    Documentation for v0.6.0

---
+++ v0.6.0/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["winreg"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"winreg/enums/enum.RegType.html\" title=\"enum winreg::enums::RegType\">RegType</a>",synthetic:false,types:["winreg::enums::RegType"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"winreg/enums/enum.RegDisposition.html\" title=\"enum winreg::enums::RegDisposition\">RegDisposition</a>",synthetic:false,types:["winreg::enums::RegDisposition"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"winreg/struct.RegValue.html\" title=\"struct winreg::RegValue\">RegValue</a>",synthetic:false,types:["winreg::RegValue"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
